a401d392dbebfba778e9ce1665b6d381604035f9
h-ikeda <h-ikeda@it-one.co.jp>
2018-06-20 11:15:14 +0900

    kerasサンプル追加

--- sapporo_weather/keras-reg.py
+++ sapporo_weather/keras-reg.py
@@ -12,45 +12,44 @@ from sklearn.pipeline import Pipeline
 from sklearn.metrics import mean_squared_error
 from sklearn.datasets import load_diabetes
 from datetime import datetime, timezone, timedelta
+from sklearn.datasets import load_diabetes
 
 # データの読み込み
-data = pd.read_csv("train.csv", index_col='date', parse_dates=True)
-print(data)
-x = (data.iloc[:, :-1]).values # 最終列以外を取得
-y = (data.iloc[:, -1:]).values # 最終列を取得
-y = np.ravel(y) # transform 2次元 to 1次元 ぽいこと
+# use diabetes sample data from sklearn
+diabetes = load_diabetes()
 
+# load them to X and Y
+# X = diabetes.data
+# Y = diabetes.target
 
+# データの読み込み
+train_data = pd.read_csv("train.csv", index_col='date', parse_dates=True)
+X = (train_data.iloc[:, :-1]).values # 最終列以外を取得
+Y = (train_data.iloc[:, -1:]).values # 最終列を取得
+Y = np.ravel(Y) # transform 2次元 to 1次元 ぽいこと
 
-# モデルの作成
-def create_model():
-    data_input = Input(shape=data.shape)
+valid_data = pd.read_csv("valid.csv", index_col='date', parse_dates=True)
+X_valid = (train_data.iloc[:, :-1]).values # 最終列以外を取得
+Y_valid = (train_data.iloc[:, -1:]).values # 最終列を取得
+Y_valid = np.ravel(Y_valid) # transform 2次元 to 1次元 ぽいこと
+
+# モデル作成
+def reg_model():
     model = Sequential()
-    model.add(Dense(10, input_dim=data_input, activation='relu'))
+    model.add(Dense(10, input_dim=4, activation='relu'))
     model.add(Dense(16, activation='relu'))
     model.add(Dense(1))
 
+    # コンパイル
     model.compile(loss='mean_squared_error', optimizer='adam')
     return model
 
-# モデル生成とビルド
-reg_model = create_model()
+# use data split and fit to run the model
+# x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.1, random_state=0)
 estimator = KerasRegressor(build_fn=reg_model, epochs=100, batch_size=10, verbose=0)
+estimator.fit(X, Y)
+y_pred = estimator.predict(X_valid)
 
-# 学習
-estimator.fit(x, y)
-
-# 学習結果を保存
-# with open('entry.pickle', 'wb') as f:
-# 	pickle.dump(clf, f)
-
-# テスト
-valid_data = pd.read_csv("valid.csv", index_col="date", parse_dates=True)
-v_x = (valid_data.iloc[:, :-1]).values
-v_y = (valid_data.iloc[:, -1:]).values
-
-y_pred = estimator.predict(v_x)
-
-# 額数データに対する適合率
-# print(result)
-# print(clf.feature_importances_)	# 各特徴量に対する寄与度を求める
+# show its root mean square error
+mse = mean_squared_error(Y_valid, y_pred)
+print("KERAS REG RMSE : %.2f" % (mse ** 0.5))
